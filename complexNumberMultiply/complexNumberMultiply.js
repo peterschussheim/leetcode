524f1332fe4faebebddffb7f86c1a743988c1774
Peter Schussheim <peter@schussheim.com>
2017-03-27 18:40:18 -0400

    start debugging complex numbers solution

--- complexNumberMultiply/complexNumberMultiply.js
+++ complexNumberMultiply/complexNumberMultiply.js
@@ -4,6 +4,24 @@
  * @return {string}
  */
 
-const complexNumberMultiply = (a, b) => {
+/**
+ * Parse input strings into 
+ */
 
+const complexNumberMultiply = (a, b) => {
+  let m = a;
+  let n = b;
+
+  [a, b] = m.replace('i', '').split('+')
+  let parsed = parseInt(a)
+  console.log(parsed);
+  let [c, d] = n.replace('i', '').split('+')
+  return [...[a, b], ...[c, d]]
+  // return (coef[0] * coef[2] - coef[1] * coef[3]) + '+' + (coef[1] * coef[2] + coef[0] * coef[3]) + 'i'
 }
+
+
+let test1 = complexNumberMultiply('1+1i', '1+1i') // '0+2i'
+let test2 = complexNumberMultiply('1+-1i', '1+-1i') // '0+-2i'
+
+console.log(test1)
