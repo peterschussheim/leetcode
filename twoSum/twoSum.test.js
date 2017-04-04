b16346d3b27c7be58b78ce4e83a83ebeb753b3e5
Peter Schussheim <peter@schussheim.com>
2017-04-04 17:41:27 -0400

    start solving problem

--- twoSum/twoSum.test.js
+++ twoSum/twoSum.test.js
@@ -1,7 +1,12 @@
+/*eslint no-undef: 0*/
+/*eslint-env node*/
+
 import twoSum from './twoSum'
 
 describe('twoSum', () => {
   test('', () => {
-    expect()
+    const nums = [3, 2, 4]
+    const target = 6
+    expect(twoSum(nums)).toBe(target)
   })
 })
