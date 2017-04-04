66911c11b260656deed8c0571ef666e7dd5ef72f
Peter Schussheim <peter@schussheim.com>
2017-04-04 18:22:12 -0400

    fix incorrect test case

--- twoSum/twoSum.test.js
+++ twoSum/twoSum.test.js
@@ -4,9 +4,14 @@
 import twoSum from './twoSum'
 
 describe('twoSum', () => {
-  test('', () => {
+  test('nums = [3, 2, 4] target = 6', () => {
     const nums = [3, 2, 4]
     const target = 6
-    expect(twoSum(nums)).toBe(target)
+    expect(twoSum(nums, target)).toEqual([1, 2])
+  })
+  test('nums = [2, 7, 11, 15] target 9', () => {
+    const nums = [2, 7, 11, 15]
+    const target = 9
+    expect(twoSum(nums, target)).toEqual([0, 1])
   })
 })
