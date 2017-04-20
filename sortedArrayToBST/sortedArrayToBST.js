43e4cb7b8467828c7fc811066ae7f011f8d54f31
Peter Schussheim <peter@schussheim.com>
2017-04-19 21:24:45 -0400

    add sortedArrayToBST

---
+++ sortedArrayToBST/sortedArrayToBST.js
@@ -0,0 +1,18 @@
+/**
+ * Given an array of elements sorted in ASCENDING order, convert to a 
+ * height-balanced BST.
+ *
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+
+const sortedArrayToBST = nums => {
+
+};
