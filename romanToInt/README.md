355d69d4ec54bbcddbe1ff357485932a862ffb4a
Peter Schussheim <peter@schussheim.com>
2017-05-22 17:37:18 -0400

    rewrite to account for use of subtractive notation

---
+++ romanToInt/README.md
@@ -0,0 +1,5 @@
+## Rules:
+  - `I` placed before `V` or `X` indicates one less, e.g. `IV` (one less than five)
+    is 4 and nine is `XI`
+  - `X` placed before `L` or `C` indicates 10 less.  Forty is `XL`.
+  - `C` placed before `D` or `M` indicates a hundred less.  400 is `CD`
--- romanToInt/romanToInt.js
+++ romanToInt/romanToInt.js
@@ -1,6 +1,12 @@
 /**
   * Given a roman numeral, convert it to an integer.
     Input is guaranteed to be within the range from 1 to 3999.
+
+    Rules:
+      - `I` placed before `V` or `X` indicates one less, e.g. `IV` (one less than five)
+        is 4 and nine is `XI`
+      - `X` placed before `L` or `C` indicates 10 less.  Forty is `XL`.
+      - `C` placed before `D` or `M` indicates a hundred less.  400 is `CD`
  */
 
 /**
@@ -19,12 +25,22 @@ const map = {
 }
 
 const romanToInt = romanNumeral => {
-  let sum = 0
-  for (let char of romanNumeral) {
-    sum += map[char]
+  const length = romanNumeral.length
+  // start building our result at the end of the roman numeral
+  let sum = map[romanNumeral[length - 1]]
+  for (let i = romanNumeral.length - 2; i >= 0; --i) {
+    if (map[romanNumeral[i]] < map[romanNumeral[i + 1]]) {
+      sum -= map[romanNumeral[i]]
+    } else {
+      sum += map[romanNumeral[i]]
+    }
   }
   return sum
 }
 
 let test = romanToInt('DCXXI')
-console.log(test)
+let test2 = romanToInt('MCMXCVI')
+console.log(
+  `${test}
+${test2}`
+)
