004a1a0e526b4c9db0b3cde6b206286c8654c10c
Peter Schussheim <peter@schussheim.com>
2017-05-26 23:50:34 -0400

    add link to digital root wiki entry

--- addDigits/addDigits.js
+++ addDigits/addDigits.js
@@ -1,6 +1,8 @@
 /*
   Given a non-negative integer num, repeatedly add all its digits until the sum has only one digit.
 
+  https://en.wikipedia.org/wiki/Digital_root
+
   For example:
     Given num = 38, the process is like:
     3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit,
