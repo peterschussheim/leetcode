dee580562a83efe3cdfdbd7e3ea079c7392ca46e
Peter Schussheim <peter@schussheim.com>
2017-06-11 12:38:01 -0400

    add helper functions, need to debug

--- palindromePermutation2/palindromePermutation2.js
+++ palindromePermutation2/palindromePermutation2.js
@@ -6,11 +6,56 @@
  */
 
 const generatePalindromes = (s: string) => {
-  let permutations = new Set(...s)
-  return Array.from(permutations)
+  let set = new Set();
+  let permutations: Map<number> = new Map();
+  let st: Array<string> = Array.from(s.length / 2);
+  let char = 0;
+  let k = 0;
+  if (!canPermutePalindrome(s, permutations)) {
+    return [];
+  }
+  for (let i = 0; i < permutations.length; i++) {
+    if (permutations[i] % 2 === 1) ch = i
+    for (let j = 0; j < permutations[i]; j++) {
+      st[k++] = i
+    }
+  }
+  permute(st, 0, ch)
 
+  return Array.from(set);
+};
 
+function canPermutePalindrome(s: string, map) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    map[s.charAt(i)]++;
+    if (map[s.charAt(i)] % 2 === 0) {
+      count--;
+    } else {
+      count++;
+    }
+  }
+  return count <= 1;
 }
-let test = generatePalindromes("abba")
-test
-export default generatePalindromes
+
+function swap(s: Array<string>, i: number, j: number) {
+  let temp = s[i];
+  s[i] = s[j];
+  s[j] = temp;
+}
+
+function permute(s: Array<string>, l: number, char: string) {
+  if (l === s.length) {
+    set.add(new String(s) + (char === 0 ? '' : char) + new String(s).reverse());
+  } else {
+    for (let i = l; i < s.length; i++) {
+      if (s[l] !== s[i] || l === i) {
+        swap(s, l, i);
+        permute(s, l + 1, char);
+        swap(s, l, i);
+      }
+    }
+  }
+}
+
+export default generatePalindromes;
