d595934c2dd94bc9fef8aa68fe1e75146874a1d8
Peter Schussheim <peter@schussheim.com>
2017-08-11 22:24:13 -0400

    start solving climbStairs

---
+++ climbStairs/climbStairs.js
@@ -0,0 +1,23 @@
+/**
+ * 
+  You are climbing a stair case. It takes n steps to reach to the top.
+
+  Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+  Note: Given n will be a positive integer.
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+const climbStairs = n => {
+  let stepsRemaining = n
+  let total
+  if (stepsRemaining === 1) return total
+  while (stepsRemaining > 0) {}
+}
+
+let test = climbStairs(1)
+test
