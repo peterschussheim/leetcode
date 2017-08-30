0906db3efc257c4fbe8e6f3f8a5a000a12932762
Peter Schussheim <peter@schussheim.com>
2017-08-29 21:24:08 -0400

    reorganize file structure

---
+++ src/arrayPairSum/index.js
@@ -0,0 +1,18 @@
+// Given an array of 2n integers, your task is to group these integers into n
+// pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes the 
+// sum of min(ai, bi) for all i from 1 to n as large as possible.
+
+// Input: [1,4,3,2]
+
+// Output: 4
+// Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+  
+};
+
+console.log(arrayPairSum([1,4,3,2]))
